ca8e6a358b4488082de60753123dea3f630fb470
Scott Robey <scottrobey@gmail.com>
2025-01-19 19:01:22 -0600

    build app scaffold

--- src/main/java/org/robey/shutdown_hook_limit/App.java
+++ src/main/java/org/robey/shutdown_hook_limit/App.java
@@ -1,4 +1,4 @@
-package org.robey.shutdwon_hook_limit;
+package org.robey.shutdown_hook_limit;
 
 
 import org.springframework.boot.SpringApplication;
@@ -7,13 +7,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.robey.utils.Log.log;
+import static org.robey.utils.Log.logError;
+
 @SpringBootApplication
 @RestController // Combines @SpringBootApplication and @RestController in one class
 @RequestMapping("/api") // Base URL for the REST endpoints
-public class ShutdownHookApp {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShutdownHookApp.class, args);
+        log(">>>>> ShutdownHookApp Starting Up >>>>>");
+        try {
+            SpringApplication.run(App.class, args);
+        } catch (Throwable t) {
+            logError("***** ShutdownHookApp Fatal Error", t);
+        } finally {
+            log(">>>>> ShutdownHookApp Exiting >>>>>");
+        }
     }
 
     @GetMapping("/hello")
---
+++ src/main/resources/shutdownhook-app.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+tar xf distributions/StackOverflowExamples-1.0-SNAPSHOT.tar
+./StackOverflowExamples-1.0-SNAPSHOT/bin/StackOverflowExamples
